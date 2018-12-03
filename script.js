81e2cf7d9967b90c7bb3ac74f193bc7fc24f78af
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-03 22:35:26 +0100

    addedInterval

---
+++ script.js
@@ -0,0 +1,21 @@
+var img;
+function init(){
+    img = document.createElement("img");
+    img.style.width = "20em";
+    img.style.display = "block";
+    img.style.marginLeft = "auto";
+    img.style.marginRight = "auto";
+    setInterval(createInterval, 1000)
+}
+
+var index = 0;
+function createInterval(){
+
+    index++
+    if(index == 3){
+        index = 0;
+    }
+    img.src = "pictures/" + index +"."+"jpg";
+    document.body.appendChild(img)
+
+}
